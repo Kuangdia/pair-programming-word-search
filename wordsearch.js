db4d42c8e713748a9abfbbc723df0e2cbc7bbd6f
Diana <slorrrr@gmail.com>
2021-12-02 00:28:01 -0500

    match the words

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,15 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+    for (let ls of horizontalJoin) {
+        if (ls.includes(word)) {
+            return true;
+        }
+    } 
+    return false;   
 }
+  
+
 
 module.exports = wordSearch
+
+//map -> creates a new array -> performs ls on every letter within the array
